f13778fbff5f0d6c36b2f605ca4d4a41d3c037a3
Mahmod Hammad <hodaman2012@gmail.com>
2020-09-24 14:18:55 +0200

    animate vertices

---
+++ sketch.js
@@ -0,0 +1,115 @@
+// Ensure ThreeJS is in global scope for the 'examples/'
+global.THREE = require("three");
+
+// Include any additional ThreeJS examples below
+require("three/examples/js/controls/OrbitControls");
+
+const canvasSketch = require("canvas-sketch");
+
+const settings = {
+  // Make the loop animated
+  animate: true,
+  // Get a WebGL canvas rather than 2D
+  context: "webgl",
+};
+
+const sketch = ({ context }) => {
+  // Create a renderer
+  const renderer = new THREE.WebGLRenderer({
+    canvas: context.canvas,
+  });
+
+  // WebGL background color
+  renderer.setClearColor("#000", 1);
+
+  // Setup a camera
+  const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 100);
+  camera.position.set(0, 0, -4);
+  camera.lookAt(new THREE.Vector3());
+
+  // Setup camera controller
+  const controls = new THREE.OrbitControls(camera, context.canvas);
+
+  // Setup your scene
+  const scene = new THREE.Scene();
+
+  // Setup a geometry
+  // ################################################################
+  // ################################################################
+  // ################################################################
+  // ################################################################
+  // ################################################################
+  // ################################################################
+  // ################################################################
+  // ################################################################
+  // ################################################################
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+  const vertexShader = /*glsl*/ `
+  varying vec2 vUv;
+  uniform float time;
+
+  void main(){
+     vec3 transformed = position;
+     transformed.x *=sin(time);
+     transformed.y *=cos(time);
+    vUv=uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(transformed.xyz, 1.0);
+  }
+`;
+
+  const fragmentShader = /* glsl*/ `
+  varying vec2 vUv;
+  uniform vec3 color;
+  uniform float time;
+  void main(){
+    float alpha=0.999;
+    gl_FragColor=vec4(sin(time),cos(time)*vUv.y,vUv.x*cos(sin(time)),alpha);
+  }
+`;
+  const material = new THREE.ShaderMaterial({
+    fragmentShader,
+    vertexShader,
+    uniforms: {
+      color: { value: new THREE.Color("green") },
+      time: { value: 0 },
+    },
+  });
+
+  // Setup a mesh with geometry + material
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  // ################################################################
+  // ################################################################
+  // ################################################################
+  // ################################################################
+  // ################################################################
+  // ################################################################
+  // ################################################################
+  // ################################################################
+  // ################################################################
+  // draw each frame
+  return {
+    // Handle resize events here
+    resize({ pixelRatio, viewportWidth, viewportHeight }) {
+      renderer.setPixelRatio(pixelRatio);
+      renderer.setSize(viewportWidth, viewportHeight, false);
+      camera.aspect = viewportWidth / viewportHeight;
+      camera.updateProjectionMatrix();
+    },
+    // Update & render your scene here
+    render({ time }) {
+      material.uniforms.time.value = time;
+      controls.update();
+      renderer.render(scene, camera);
+    },
+    // Dispose of events & renderer for cleaner hot-reloading
+    unload() {
+      controls.dispose();
+      renderer.dispose();
+    },
+  };
+};
+
+canvasSketch(sketch, settings);
